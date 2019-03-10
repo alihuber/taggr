ae4b60204598514baca47abb362e1f8794b04e87
Alexander Huber <alih83@gmx.de>
2019-03-10 16:29:18 +0100

    add material-ui

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,39 @@
 import '../assets/css/App.css';
 import React from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+
+const styles = {
+  root: {
+    flexGrow: 1,
+  },
+  flex: {
+    flexGrow: 1,
+    cursor: 'pointer',
+  },
+  menuButton: {
+    marginLeft: -12,
+    marginRight: 20,
+  },
+};
 
 class App extends React.Component {
   render() {
+    const { classes } = this.props;
     return (
-      <div>
-        <h1>Hello, Electron!</h1>
-        <p>I hope you enjoy using basic-electron-react-boilerplate to start your dev off right!</p>
+      <div className={classes.root}>
+        <AppBar position="static">
+          <Toolbar>
+            <Typography variant="h6" color="inherit" className={classes.flex}>
+              Home
+            </Typography>
+          </Toolbar>
+        </AppBar>
       </div>
     );
   }
 }
 
-export default App;
+export default withStyles(styles)(App);
